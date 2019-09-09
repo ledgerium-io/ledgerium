e0f3386a71774b2b916a7e22b5e82f706030a5f7
victoria <kapitonenko.vick@ya.ru>
2019-09-09 23:33:40 +0300

    fix: load logotype when scrolling

--- js/header.js
+++ js/header.js
@@ -1,15 +1,17 @@
 window.onscroll = () => {
   const header = document.querySelector('.header');
   const box = document.querySelector('.header__box');
-  const logo = document.querySelector('.header__logo-img');
+  const logo = document.querySelectorAll('.header__logo-img');
 
   if (window.pageYOffset > 0) {
     header.classList.add('header_scroll');
     box.classList.add('header__box_scroll');
-    logo.src = 'img/logotype-dark.svg';
+    logo[0].style.display = 'none';
+    logo[1].style.display = 'inline-block';
   } else { 
     header.classList.remove('header_scroll');
     box.classList.remove('header__box_scroll');
-    logo.src = 'img/logotype.svg';
+    logo[0].style.display = 'inline-block';
+    logo[1].style.display = 'none';
   }
 }
