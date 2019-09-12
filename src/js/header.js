0d9a974746e2bc59e7fb570d79a1dc7be188c8c5
victoria <kapitonenko.vick@ya.ru>
2019-09-12 20:29:40 +0300

    fix: work of scroll class for header

--- src/js/header.js
+++ src/js/header.js
@@ -1,34 +1,9 @@
-const headerHandle = () => {
-  const headerWrap = document.querySelector('.header');
-  const box = document.querySelector('.header__box');
-  const logo = document.querySelectorAll('.header__logo-img');
-
-  if (window.pageYOffset > 0) {
-    headerWrap.classList.add('header_scroll');
-    box.classList.add('header__box_scroll');
-    logo[0].style.display = 'none';
-    logo[1].style.display = 'inline-block';
-  } else {
-    headerWrap.classList.remove('header_scroll');
-    box.classList.remove('header__box_scroll');
-    logo[0].style.display = 'inline-block';
-    logo[1].style.display = 'none';
-  }
-}
-
-window.onscroll = () => {
-  headerHandle();
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  headerHandle();
-});
-
 var headerContainer = new Vue({
   el: '.header',
   data: {
     isShowMenu: false,
-    isShowHeight: false
+    isShowHeight: false,
+    isScroll: false
   },
   methods: {
     toggleMenu() {
@@ -41,7 +16,14 @@ var headerContainer = new Vue({
         this.isShowMenu = true;
         this.isShowHeight = true;
       }
-      headerHandle();
+    },
+    headerHandle() {
+      this.isScroll = window.pageYOffset > 0;
+    }
+  },
+  mounted() {
+    window.onscroll = () => {
+      this.headerHandle();
     }
   }
 })
