8ace1f9a5650744c15423ed5d8cafa09305b087e
victoria <kapitonenko.vick@ya.ru>
2019-09-12 20:04:47 +0300

    feat: mobile version

--- src/js/header.js
+++ src/js/header.js
@@ -1,17 +1,47 @@
-window.onscroll = () => {
-  const header = document.querySelector('.header');
+const headerHandle = () => {
+  const headerWrap = document.querySelector('.header');
   const box = document.querySelector('.header__box');
   const logo = document.querySelectorAll('.header__logo-img');
 
   if (window.pageYOffset > 0) {
-    header.classList.add('header_scroll');
+    headerWrap.classList.add('header_scroll');
     box.classList.add('header__box_scroll');
     logo[0].style.display = 'none';
     logo[1].style.display = 'inline-block';
-  } else { 
-    header.classList.remove('header_scroll');
+  } else {
+    headerWrap.classList.remove('header_scroll');
     box.classList.remove('header__box_scroll');
     logo[0].style.display = 'inline-block';
     logo[1].style.display = 'none';
   }
 }
+
+window.onscroll = () => {
+  headerHandle();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  headerHandle();
+});
+
+var headerContainer = new Vue({
+  el: '.header',
+  data: {
+    isShowMenu: false,
+    isShowHeight: false
+  },
+  methods: {
+    toggleMenu() {
+      if (this.isShowMenu) {
+        this.isShowHeight = false;
+        setTimeout(() => {
+          this.isShowMenu = false;
+        }, 100);
+      } else {
+        this.isShowMenu = true;
+        this.isShowHeight = true;
+      }
+      headerHandle();
+    }
+  }
+})
