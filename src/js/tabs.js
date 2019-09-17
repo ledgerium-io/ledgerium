ee4535736275953f07e3e484a164db95c37f0271
victoria <kapitonenko.vick@ya.ru>
2019-09-17 19:48:07 +0300

    feat: add highligt current block on page

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,15 +1,44 @@
 {
   const links = document.querySelectorAll('.xgl-top a');
+
+  const classHandle = (link) => {
+    for (let k = 0; k < links.length; k++) {
+      links[k].classList.remove('tabs__item_active');
+    }
+    link.classList.add('tabs__item_active');
+  }
+
+  const currentLinkHandle = () => {
+    for (let i = 0; (i < links.length - 1); i++) {
+      let nextDivOffset = document.querySelector('.the-way__box').clientHeight;
+      if (links[i + 1].hash) {
+        nextDivOffset = document.querySelector(links[i + 1].hash).offsetTop;
+      }
+      const divOffset = document.querySelector(links[i].hash).offsetTop;
+
+      if (window.pageYOffset > divOffset && window.pageYOffset < nextDivOffset) {
+        classHandle(links[i]);
+      }
+    }
+  }
+
   for (let i = 0; i < (links.length - 1); i++) {
     links[i].addEventListener('click', (e) => {
       e.preventDefault();
-      for (let k = 0; k < links.length; k++) {
-        links[k].classList.remove('tabs__item_active');
-      }
-      e.target.classList.add('tabs__item_active');
+
+      classHandle(e.target);
+      
       const id = e.target.hash;
       const div = document.querySelector(id);
       div.scrollIntoView({ behavior: 'smooth', block: 'start' });
     });
   }
+
+  window.onscroll = () => {
+    currentLinkHandle();
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    currentLinkHandle();
+  });
 }
