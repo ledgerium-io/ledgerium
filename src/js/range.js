7e16e8cba601122c6a09b2e11ec4ba7ee3b6148c
victoria <kapitonenko.vick@ya.ru>
2019-09-18 18:06:36 +0300

    feat: add pricing top and plans (active and unactive)

--- src/js/range.js
+++ src/js/range.js
@@ -1,6 +1,28 @@
 var range = new Vue({
-  el: '.range',
+  el: '.pricing',
   data: {
-    current: 0
+    current: 0,
+    prices: [
+      {
+        period: 'Q1 & Q2 2019',
+        title: 'Testnet'
+      },
+      {
+        period: 'Q3 2019',
+        title: 'Mainnet'
+      },
+      {
+        period: 'Q1 2020',
+        title: 'Services'
+      },
+      {
+        period: 'Q2 2020',
+        title: 'Cross Chain Tx'
+      },
+      {
+        period: 'Q4 2020',
+        title: 'Scaling'
+      }
+    ]
   },
 });
