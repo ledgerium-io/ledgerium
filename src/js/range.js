e037fce32065a2c6f5127a38aa5947c7383feeff
victoria <kapitonenko.vick@ya.ru>
2019-09-19 11:59:59 +0300

    feat: mobile version of pricing section

--- src/js/range.js
+++ src/js/range.js
@@ -1,7 +1,7 @@
 var range = new Vue({
   el: '.pricing',
   data: {
-    current: 0,
+    current: '0',
     prices: [
       {
         period: 'Q1 & Q2 2019',
@@ -35,11 +35,31 @@ var range = new Vue({
       }
     },
     sliderHandle() {
-      document.addEventListener('mousemove', this.sliderMove);
-      document.addEventListener('mouseup', this.sliderLeave);
+      if (document.body.clientWidth > 840) {
+        document.addEventListener('mousemove', this.sliderMove);
+        document.addEventListener('mouseup', this.sliderLeave);
+      }
     },
     sliderLeave() {
       document.removeEventListener('mousemove', this.sliderMove);
+    }
+  },
+  computed: {
+    progress() {
+      if (document.body.clientWidth <= 840) {
+        switch(this.current) {
+          case '1':
+            return '10%';
+          case '2':
+            return '25%'; 
+          case '3':
+            return '52%'; 
+          case '4':
+            return '67%'; 
+        }
+      }
+
+      return 25 * this.current + '%';
     }
   }
 });
