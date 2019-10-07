9883ee780e98720320b7d40fa96bf25a759dd6c4
victoria <kapitonenko.vick@ya.ru>
2019-10-07 16:55:37 +0300

    feat: change working tabs

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,5 +1,6 @@
 {
   const links = document.querySelectorAll('.xlg-top a');
+  const box = document.querySelector('.xlg-way__box');
 
   const classHandle = (link) => {
     for (let k = 0; k < links.length; k++) {
@@ -8,57 +9,15 @@
     link.classList.add('tabs__item_active');
   }
 
-  const currentLinkHandle = (box) => {
-      for (let i = 0; (i < links.length - 1); i++) {
-        let nextDivOffset = box.clientHeight;
-        if (box) {
-          nextDivOffset = box.scrollWidth;
-        }
-
-        if (links[i + 1].hash) {
-          nextDivOffset = document.querySelector(links[i + 1].hash).offsetTop + 150;
-
-          if (box) {
-            nextDivOffset = document.querySelector(links[i + 1].hash).offsetLeft - 60;
-          }
-        }
-
-        let divOffset = document.querySelector(links[i].hash).offsetTop + 150;
-
-        if (box) {
-          divOffset = document.querySelector(links[i].hash).offsetLeft - 60;
-        }
-
-        if (box) {
-          if (box.scrollLeft > divOffset && box.scrollLeft < nextDivOffset) {
-            classHandle(links[i]);
-          }
-        } else if (window.pageYOffset > divOffset && window.pageYOffset < nextDivOffset) {
-          classHandle(links[i]);
-        }
-      }
-  }
-
   for (let i = 0; i < (links.length - 1); i++) {
     links[i].addEventListener('click', (e) => {
       e.preventDefault();
-
-      classHandle(e.target);
-      
-      const id = e.target.hash;
-      const div = document.querySelector(id);
-      div.scrollIntoView({ behavior: 'smooth' });
+      const width = document.documentElement.clientWidth;
+      box.style.transform = `translateX(${-i * width}px)`;
+      classHandle(links[i]);
     });
   }
 
-  const box = document.querySelector('.xlg-way__box');
-
-  box.addEventListener('scroll', () => {
-    currentLinkHandle(box);
-    console.log(box.scrollLeft);
-  });
-
   document.addEventListener('DOMContentLoaded', () => {
-    currentLinkHandle(box);
   });
 }
